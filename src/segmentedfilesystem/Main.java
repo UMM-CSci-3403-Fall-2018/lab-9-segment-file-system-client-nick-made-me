660eb8413a882b9c3f12f3ff00d90f0e68426c53
M1NN <score@morris.umn.edu>
2018-11-27 11:20:57 -0600

    Move server initialization code to separate method and start work on receiving packets from server

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,30 +1,41 @@
 package segmentedfilesystem;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 
 public class Main {
     
     public static void main(String[] args) {
-        byte[] buf = new byte[0];
         DatagramSocket socket;
-        DatagramPacket emptyPacket;
         final InetAddress address;
 
         try {
-            address = InetAddress.getByName(args[0]);
             socket = new DatagramSocket();
-            emptyPacket = new DatagramPacket(buf, buf.length, address, 6014);
-            socket.send(emptyPacket);
-        } catch (SocketException e) {
+            address = InetAddress.getByName(args[0]);
+            initConnection(address, socket);
+        } catch (UnknownHostException e) {
             System.out.println("Die");
-        } catch (IOException e) {
+        } catch (SocketException e) {
             System.out.println("Die");
         }
 
+    }
+
+    public static void initConnection(InetAddress address, DatagramSocket socket) {
+        byte[] buf = new byte[0];
+        DatagramPacket emptyPacket;
+
+        try {
+            emptyPacket = new DatagramPacket(buf, buf.length, address, 6014);
+            socket.send(emptyPacket);
+        } catch (IOException e) {
+            System.out.println("Die");
+        }
+    }
+
+    public static boolean receivePackets(DatagramSocket socket) {
+        return false;
     }
 
 }
