fe536d7ddf4b8e60dd449760a062a00e80f7a99b
M1NN <score@morris.umn.edu>
2018-11-27 10:56:49 -0600

    Add functionality to send a packet with an empty buffer to the server to open the flood gate

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,30 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Main {
     
     public static void main(String[] args) {
-        
+        byte[] buf = new byte[0];
+        DatagramSocket socket;
+        DatagramPacket emptyPacket;
+        final InetAddress address;
+
+        try {
+            address = InetAddress.getByName(args[0]);
+            socket = new DatagramSocket();
+            emptyPacket = new DatagramPacket(buf, buf.length, address, 6014);
+            socket.send(emptyPacket);
+        } catch (SocketException e) {
+            System.out.println("Die");
+        } catch (IOException e) {
+            System.out.println("Die");
+        }
+
     }
 
 }
