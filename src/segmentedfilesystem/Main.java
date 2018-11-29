9119e1170c8bece7509f47d5b297b2640829eb04
Nick Plucker <pluck011@morris.umn.edu>
2018-11-29 11:26:56 -0600

    Continue work on receiving packets from the server
    Add HashMap to determine what packet goes to which file and start work on filtering the packets to their respective files.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 public class Main {
     
     public static void main(String[] args) throws IOException {
+
         DatagramSocket socket;
         final InetAddress address;
         HashMap<Byte, UDPfile> fileMap = new HashMap<Byte, UDPfile>();
@@ -27,34 +28,34 @@ public class Main {
         socket.send(emptyPacket);
     }
 
-    public static boolean receivePackets(DatagramSocket socket, HashMap<Byte, UDPfile> fileMap) throws IOException {
+    public static void receivePackets(DatagramSocket socket, HashMap<Byte, UDPfile> fileMap) throws IOException {
         int headerCount = 0;
         int endCount = 0;
         boolean filesDone = false;
+        UDPfile file1 = new UDPfile();
+        UDPfile file2 = new UDPfile();
+        UDPfile file3 = new UDPfile();
 
-        while(headerCount != 3 && endCount != 3 && !filesDone) {
+        while(!(file1.isComplete()) && !(file2.isComplete()) && !(file3.isComplete())) {
             byte[] buf = new byte[1028];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
 
             if (buf[0] % 2 == 0) {
                 headerCount++;
+                if(fileMap.get(buf[1]) == null) {
+                    fileMap.put(buf[1], new UDPfile());
+                }
+                fileMap.get(buf[1]).add(packet, true, false, 0);
             }
 
             if(buf[0] % 2 != 0) {
-                if (buf[1] % 2 != 0) {
+                if (buf[0] == (3 % 4)) {
                     endCount++;
                 }
             }
 
-
-
-
-
         }
-
-
-
     }
 
 }
--- src/segmentedfilesystem/UDPfile.java
+++ src/segmentedfilesystem/UDPfile.java
@@ -4,7 +4,31 @@ import java.net.DatagramPacket;
 import java.util.ArrayList;
 
 public class UDPfile {
+    public boolean hasHead;
+    public boolean hasEnd;
+    public int fileLength;
     public ArrayList<DatagramPacket> packetData = new ArrayList<DatagramPacket>();
+
+    public void add(DatagramPacket packet, boolean isHead, boolean isEnd, int numOfPackets) {
+        if(isHead) {
+            this.hasHead = true;
+        }
+
+        if(isEnd) {
+            this.hasEnd = true;
+            this.fileLength = numOfPackets;
+        }
+
+        this.packetData.add(packet);
+    }
+
+    public boolean isComplete() {
+        if (this.hasHead && this.hasEnd) {
+            return (packetData.size() == this.fileLength);
+        } else {
+            return false;
+        }
+    }
 
 
 }
