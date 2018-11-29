3afd43bdaccda6ac26c87b0c78dcda3bac1681ab
Nick Plucker <pluck011@morris.umn.edu>
2018-11-29 10:41:09 -0600

    Start work on receiving packets from the server
    Add checks to determine when the socket is done sending data. These have not been tested and as of this commit, as this is still a work in progress.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,41 +1,60 @@
 package segmentedfilesystem;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.*;
+import java.util.HashMap;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DatagramSocket socket;
         final InetAddress address;
+        HashMap<Byte, UDPfile> fileMap = new HashMap<Byte, UDPfile>();
 
-        try {
-            socket = new DatagramSocket();
-            address = InetAddress.getByName(args[0]);
-            initConnection(address, socket);
-        } catch (UnknownHostException e) {
-            System.out.println("Die");
-        } catch (SocketException e) {
-            System.out.println("Die");
-        }
+        socket = new DatagramSocket();
+        address = InetAddress.getByName(args[0]);
+
+        initConnection(address, socket);
+        receivePackets(socket, fileMap);
 
     }
 
-    public static void initConnection(InetAddress address, DatagramSocket socket) {
+    public static void initConnection(InetAddress address, DatagramSocket socket) throws IOException {
         byte[] buf = new byte[0];
         DatagramPacket emptyPacket;
 
-        try {
-            emptyPacket = new DatagramPacket(buf, buf.length, address, 6014);
-            socket.send(emptyPacket);
-        } catch (IOException e) {
-            System.out.println("Die");
-        }
+        emptyPacket = new DatagramPacket(buf, buf.length, address, 6014);
+        socket.send(emptyPacket);
     }
 
-    public static boolean receivePackets(DatagramSocket socket) {
-        return false;
+    public static boolean receivePackets(DatagramSocket socket, HashMap<Byte, UDPfile> fileMap) throws IOException {
+        int headerCount = 0;
+        int endCount = 0;
+        boolean filesDone = false;
+
+        while(headerCount != 3 && endCount != 3 && !filesDone) {
+            byte[] buf = new byte[1028];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+
+            if (buf[0] % 2 == 0) {
+                headerCount++;
+            }
+
+            if(buf[0] % 2 != 0) {
+                if (buf[1] % 2 != 0) {
+                    endCount++;
+                }
+            }
+
+
+
+
+
+        }
+
+
+
     }
 
 }
---
+++ src/segmentedfilesystem/UDPfile.java
@@ -0,0 +1,10 @@
+package segmentedfilesystem;
+
+import java.net.DatagramPacket;
+import java.util.ArrayList;
+
+public class UDPfile {
+    public ArrayList<DatagramPacket> packetData = new ArrayList<DatagramPacket>();
+
+
+}
