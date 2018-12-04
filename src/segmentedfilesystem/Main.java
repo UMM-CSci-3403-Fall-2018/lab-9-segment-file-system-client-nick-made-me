c7c8fa47e1d3d14c0ce83f3a20546c433c224688
Nick Plucker <pluck011@morris.umn.edu>
2018-12-04 11:11:22 -0600

    Successfully receive and filter all packets
    The packets successfully get sorted with their respective file. Next is to sort them and write them out.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -2,6 +2,7 @@ package segmentedfilesystem;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
@@ -18,6 +19,9 @@ public class Main {
         initConnection(address, socket);
         receivePackets(socket, fileMap);
 
+        System.out.println("done with packets, closing socket");
+
+        socket.close();
     }
 
     public static void initConnection(InetAddress address, DatagramSocket socket) throws IOException {
@@ -28,34 +32,70 @@ public class Main {
         socket.send(emptyPacket);
     }
 
+    public static int calculateSize(int largeInt, int smallInt) {
+        System.out.println(largeInt + " " + smallInt);
+        if (largeInt < 0) {
+            largeInt = largeInt + 256;
+        }
+        if (smallInt < 0) {
+            smallInt = smallInt + 256;
+        }
+        return largeInt * 256 + smallInt;
+    }
+
+    public static boolean filesDone(ArrayList<Byte> fileIDs, HashMap<Byte, UDPfile> fileMap) {
+        for (int i = 0; i < fileIDs.size(); i++) {
+            if(!(fileMap.get(fileIDs.get(i)).isComplete())) {
+                return false;
+            }
+        }
+
+        return (fileIDs.size() == 3);
+    }
+
     public static void receivePackets(DatagramSocket socket, HashMap<Byte, UDPfile> fileMap) throws IOException {
         int headerCount = 0;
         int endCount = 0;
         boolean filesDone = false;
-        UDPfile file1 = new UDPfile();
-        UDPfile file2 = new UDPfile();
-        UDPfile file3 = new UDPfile();
+        ArrayList<Byte> fileIDs = new ArrayList<Byte>();
 
-        while(!(file1.isComplete()) && !(file2.isComplete()) && !(file3.isComplete())) {
+        while(!filesDone(fileIDs, fileMap)) {
+          //  System.out.println("Im in the loop");
             byte[] buf = new byte[1028];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
+           // System.out.println("The size of the map is " + fileMap.size());
 
             if (buf[0] % 2 == 0) {
+                //System.out.println("Im in the header if");
                 headerCount++;
                 if(fileMap.get(buf[1]) == null) {
+                    fileIDs.add(buf[1]);
                     fileMap.put(buf[1], new UDPfile());
                 }
                 fileMap.get(buf[1]).add(packet, true, false, 0);
             }
 
             if(buf[0] % 2 != 0) {
+              //  System.out.println("Im in the data if");
                 if (buf[0] == (3 % 4)) {
+                    System.out.println("Im in the ender if");
+                    if(fileMap.get(buf[1]) == null) {
+                        fileIDs.add(buf[1]);
+                        fileMap.put(buf[1], new UDPfile());
+                    }
+                    fileMap.get(buf[1]).add(packet, false, true, calculateSize(buf[2], buf[3]));
                     endCount++;
+                } else {
+                    if (fileMap.get(buf[1]) == null) {
+                        fileIDs.add(buf[1]);
+                        fileMap.put(buf[1], new UDPfile());
+                    }
+                    fileMap.get(buf[1]).add(packet, false, false, 0);
                 }
             }
-
         }
-    }
 
+
+    }
 }
--- src/segmentedfilesystem/UDPfile.java
+++ src/segmentedfilesystem/UDPfile.java
@@ -10,16 +10,20 @@ public class UDPfile {
     public ArrayList<DatagramPacket> packetData = new ArrayList<DatagramPacket>();
 
     public void add(DatagramPacket packet, boolean isHead, boolean isEnd, int numOfPackets) {
+       // System.out.println("Im in add");
+
         if(isHead) {
             this.hasHead = true;
         }
 
         if(isEnd) {
             this.hasEnd = true;
-            this.fileLength = numOfPackets;
+            this.fileLength = numOfPackets + 2;
         }
 
         this.packetData.add(packet);
+        System.out.println(this.hasHead + " " + this.hasEnd + " " + this.fileLength + " " + this.isComplete());
+       // System.out.println(packetData.size()+ " " + this.fileLength);
     }
 
     public boolean isComplete() {
